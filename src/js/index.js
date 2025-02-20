43d2f524e687e4a2fa1daebe16416f2dfd55d6f7
e3dr8nj <hy421725@gmail.com>
2025-02-20 17:08:13 +0300

    test

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import bar from './bar.js';
+import '../scss/styles.css'; // ️
+import * as bootstrap from 'bootstrap';//
+bar();
---
+++ src/js/index.ts
@@ -0,0 +1,15 @@
+
+
+//import bar from './bar.js';
+import '../scss/styles.css'; // ️
+//import * as bootstrap from 'bootstrap';//
+const user = {
+    firstName: 'Angela',
+    lastName: 'Davis',
+      role: 'Professor',
+  };
+   
+  console.log(user.role);
+ // document.body.innerHTML='3';
+   console.log('m');
+  console.log('Hello World');
